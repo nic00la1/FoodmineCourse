6ee3c77322a56ff1f6786f88ea9deb816ba49cf7
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-03-21 12:01:55 +0100

    star-rating ✨

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @Component({
   selector: 'app-root',
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { FoodService } from '../services/food/food.service';
-import { Food } from '../shared/models/Food.model';
-import { StarRatingModule } from 'angular-star-rating';
+import { FoodService } from '../../../services/food/food.service';
+import { Food } from '../../../shared/models/Food.model';
+import { StarRatingComponent } from '../../partials/star-rating/star-rating.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [StarRatingModule],
+  imports: [StarRatingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
