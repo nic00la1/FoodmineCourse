c18c5695c3ec54cff759d6edccdde039221f7135
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-03-21 13:14:25 +0100

    Adding food details to the Cards ✨

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { FoodService } from '../../../services/food/food.service';
 import { Food } from '../../../shared/models/Food.model';
 import { StarRatingComponent } from '../../partials/star-rating/star-rating.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [StarRatingComponent],
+  imports: [StarRatingComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
