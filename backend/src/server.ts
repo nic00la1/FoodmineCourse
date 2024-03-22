17cbb89cea54b6bb2ac47a9c3252a5cfd9ace57d
nic00la <nic00la.kaleta@gmail.com>
2024-03-22 12:48:24 +0100

    Adding API's

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,50 @@
 import express from "express";
 import cors from "cors";
-import { sample_foods } from "./data";
+import { sample_foods, sample_tags } from "./data";
 
 const app = express();
 // localhost:4200 is the default port for Angular
 // localhost:5000 we will use for our server
 
 // thats why:
-app.use( 
+app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:4200", 
+    origin: "http://localhost:4200",
   })
 );
 
 app.get("/api/foods", (req, res) => {
-    res.send(sample_foods);
-})
+  res.send(sample_foods);
+});
+
+app.get("/api/foods/search/:searchTerm", (req, res) => {
+  // :searchTerm is a route parameter
+  const searchTerm = req.params.searchTerm;
+  const foods = sample_foods.filter((food) =>
+    food.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  res.send(foods);
+});
+
+app.get("/api/foods/tags", (req, res) => {
+  res.send(sample_tags);
+});
+
+app.get("/api/foods/tag/:tagName", (req, res) => {
+  const tagName = req.params.tagName;
+  const foods = sample_foods
+    .filter((food) => food.tags?.includes(tagName));
+    res.send(foods);
+});
+
+app.get("/api/foods/:foodId", (req, res) => {
+  const foodId = req.params.foodId;
+  const food = sample_foods.find(food => food.id == foodId) 
+  res.send(food);
+});
 
 const port = 5000; // default port to listen
 app.listen(port, () => {
-    console.log("server started at http://localhost:" + port);
-})
+  console.log("server started at http://localhost:" + port);
+});
