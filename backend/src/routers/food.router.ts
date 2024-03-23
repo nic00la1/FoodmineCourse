c49d94c32c1cc602047c46c898f46aad99a40921
nic00la <nic00la.kaleta@gmail.com>
2024-03-23 14:12:49 +0100

    Use Database instead of data.ts

--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -19,33 +19,71 @@ router.get(
   })
 );
 
-router.get("/", (req, res) => {
-  res.send(sample_foods);
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const foods = await FoodModel.find();
+    res.send(foods);
+  })
+);
 
-router.get("/search/:searchTerm", (req, res) => {
-  // :searchTerm is a route parameter
-  const searchTerm = req.params.searchTerm;
-  const foods = sample_foods.filter((food) =>
-    food.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  res.send(foods);
-});
+router.get(
+  "/search/:searchTerm",
+  asyncHandler(async (req, res) => {
+    const searchRegex = new RegExp(req.params.searchTerm, "i");
+    const foods = await FoodModel.find({
+      name: { $regex: searchRegex },
+    });
+    res.send(foods);
+  })
+);
 
-router.get("/tags", (req, res) => {
-  res.send(sample_tags);
-});
+router.get(
+  "/tags",
+  asyncHandler(async (req, res) => {
+    const tags = await FoodModel.aggregate([
+      {
+        $unwind: "$tags",
+      },
+      {
+        $group: {
+          _id: "$tags",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          name: "$_id",
+          count: "$count",
+        },
+      },
+    ]).sort({ count: -1 });
 
-router.get("/tag/:tagName", (req, res) => {
-  const tagName = req.params.tagName;
-  const foods = sample_foods.filter((food) => food.tags?.includes(tagName));
-  res.send(foods);
-});
+    const all = {
+      name: "All",
+      count: await FoodModel.countDocuments(),
+    };
 
-router.get("/:foodId", (req, res) => {
-  const foodId = req.params.foodId;
-  const food = sample_foods.find((food) => food.id == foodId);
-  res.send(food);
-});
+    tags.unshift(all); // Add all tag to the beginning of the array
+    res.send(tags);
+  })
+);
+
+router.get(
+  "/tag/:tagName",
+  asyncHandler(async (req, res) => {
+    const foods = await FoodModel.find({ tags: req.params.tagName });
+    res.send(foods);
+  })
+);
+
+router.get(
+  "/:foodId",
+  asyncHandler(async (req, res) => {
+    const food = await FoodModel.findById(req.params.foodId);
+    res.send(food);
+  })
+);
 
 export default router;
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { sample_foods, sample_users } from "../data";
-import jwt  from 'jsonwebtoken';
-import asyncHandler  from 'express-async-handler';
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
 import { UserModel } from "../models/user.model";
 
 const router = Router();
@@ -20,25 +20,33 @@ router.get(
   })
 );
 
-router.post("/login", (req, res) => {
-    const {email, password} = req.body;
-    const user = sample_users.find(user => user.email === email &&
-     user.password === password);
-  
-     if(user) {
-        res.send(generateTokenResponse(user));
-     } else {
-        res.status(400).send("User name or password is not valid!");
-     }
-  });
-  
-  const generateTokenResponse = (user: any) => {
-    const token = jwt.sign({
-      email: user.email, isAdmin: user.isAdmin
-    }, "SomeRandomText", {expiresIn: "30d"});
-  
-    user.token = token;
-    return user;
-  }
-  
-  export default router;
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email, password });
+
+    if (user) {
+      res.send(generateTokenResponse(user));
+    } else {
+      const BAD_REQUEST = 400;
+      res.status(BAD_REQUEST).send("Username or password is not valid!");
+    }
+  })
+);
+
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign(
+    {
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    "SomeRandomText",
+    { expiresIn: "30d" }
+  );
+
+  user.token = token;
+  return user;
+};
+
+export default router;
