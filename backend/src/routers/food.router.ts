b4743d9977d96f8c0467b666a2f4e57c3b57d59b
nic00la <nic00la.kaleta@gmail.com>
2024-03-23 13:40:03 +0100

    Add Seed API's ✅

--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -1,7 +1,23 @@
 import { Router } from "express";
 import { sample_foods, sample_tags } from "../data";
+import asyncHandler from "express-async-handler";
+import { FoodModel } from "../models/food.models";
 
 const router = Router();
+
+router.get(
+  "/seed",
+  asyncHandler(async (req, res) => {
+    const foodsCount = await FoodModel.countDocuments();
+    if (foodsCount > 0) {
+      res.send("Seed is already done");
+      return;
+    }
+
+    await FoodModel.create(sample_foods);
+    res.send("Seed is done!");
+  })
+);
 
 router.get("/", (req, res) => {
   res.send(sample_foods);
