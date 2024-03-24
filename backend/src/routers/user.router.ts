3231f99ab40213cf0bf76d06b99d390c9e474a8f
nic00la <nic00la.kaleta@gmail.com>
2024-03-24 13:51:17 +0100

    Adding register API ✅

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -3,6 +3,8 @@ import { sample_users } from "../data";
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
@@ -29,11 +31,34 @@ router.post(
     if (user) {
       res.send(generateTokenResponse(user));
     } else {
-      const BAD_REQUEST = 400;
-      res.status(BAD_REQUEST).send("Username or password is not valid!");
+      res.status(HTTP_BAD_REQUEST).send("Username or password is not valid!");
     }
   })
 );
+
+router.post('/register', asyncHandler(async (req, res) => {
+  const { name, email, password, address } = req.body;
+  const user = await UserModel.create({email});
+  if(user) {
+    res.status(HTTP_BAD_REQUEST)
+    .send("User already exists, please login!");
+    return;
+  }
+
+  const encryptedPassword = await bcrypt.hash(password, 10)
+
+  const newUser : User = {
+    id: '',
+    name,
+    email: email.toLowerCase(),
+    password: encryptedPassword,
+    address,
+    isAdmin: false
+  }
+
+  const dbUser = await UserModel.create(newUser);
+  res.send(generateTokenResponse(dbUser));
+}));
 
 const generateTokenResponse = (user : User) => {
   const token = jwt.sign({
