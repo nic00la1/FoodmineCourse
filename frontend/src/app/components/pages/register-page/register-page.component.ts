e73a7eeefdb8cf64cebd781f33856aa15c7e96fb
nic00la <nic00la.kaleta@gmail.com>
2024-03-24 14:17:27 +0100

    register component's submit method

--- frontend/src/app/components/pages/register-page/register-page.component.ts
+++ frontend/src/app/components/pages/register-page/register-page.component.ts
@@ -4,16 +4,17 @@ import { TitleComponent } from '../../partials/title/title.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../../services/user/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IUserRegister } from '../../../shared/interfaces/IUserRegister';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
   imports: [TitleComponent],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
-  registerForm !: FormGroup
+  registerForm!: FormGroup;
   isSubmitted = false;
 
   returnUrl = '';
@@ -23,18 +24,42 @@ export class RegisterPageComponent {
   private activatedRoute = inject(ActivatedRoute);
   private router = inject(Router);
 
-  ngOnInit() : void {
-    this.registerForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(5)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(5)]],
-      confirmPassword: ['', Validators.required],
-      address: ['', [Validators.required, Validators.minLength(10)]],
-    },{
-      validators: PasswordsMatchValidator('password', 'confirmPassword')
-    });
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(5)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(5)]],
+        confirmPassword: ['', Validators.required],
+        address: ['', [Validators.required, Validators.minLength(10)]],
+      },
+      {
+        validators: PasswordsMatchValidator('password', 'confirmPassword'),
+      }
+    );
 
     this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
   }
 
+  get fc() {
+    return this.registerForm.controls;
+  }
+
+  submit() {
+    this.isSubmitted = true;
+    if (this.registerForm.invalid) return;
+
+    const fv = this.registerForm.value;
+    const user: IUserRegister = {
+      name: fv.name,
+      email: fv.email,
+      password: fv.password,
+      confirmPassword: fv.confirmPassword,
+      address: fv.address,
+    };
+
+    this.userService.register(user).subscribe((_) => {
+      this.router.navigateByUrl(this.returnUrl);
+    });
+  }
 }
