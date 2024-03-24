f3c83ccebc89bf2768a343fcdf3f011009a2568b
nic00la <nic00la.kaleta@gmail.com>
2024-03-24 14:06:11 +0100

    Defining register form

---
+++ frontend/src/app/components/pages/register-page/register-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+import { TitleComponent } from '../../partials/title/title.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../../services/user/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: true,
+  imports: [TitleComponent],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css'
+})
+export class RegisterPageComponent {
+  registerForm !: FormGroup
+  isSubmitted = false;
+
+  returnUrl = '';
+
+  private formBuilder = inject(FormBuilder);
+  private userService = inject(UserService);
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+
+  ngOnInit() : void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      confirmPassword: ['', Validators.required],
+      address: ['', [Validators.required, Validators.minLength(10)]],
+    });
+
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
+  }
+}
