1479fdc8eb67bf49da45b029136ef62cd0ec698d
nic00la <nic00la.kaleta@gmail.com>
2024-03-25 08:52:39 +0100

    Adding Find my location feature ✅

--- frontend/src/app/components/partials/map/map.component.ts
+++ frontend/src/app/components/partials/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { LatLngTuple, Map, map, tileLayer } from 'leaflet';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { LatLng, LatLngExpression, LatLngTuple, LeafletMouseEvent, Map, Marker, icon, map, marker, tileLayer } from 'leaflet';
+import { LocationService } from '../../../services/location/location.service';
 
 @Component({
   selector: 'map',
@@ -10,12 +11,20 @@ import { LatLngTuple, Map, map, tileLayer } from 'leaflet';
 })
 export class MapComponent {
 
+  private readonly MARKER_ZOOM_LEVEL = 16;
+  private readonly MARKER_ICON = icon({
+    iconUrl: 'https://res.cloudinary.com/foodmine/image/upload/v1638842791/map/marker_kbua9q.png',
+    iconSize: [42, 42],
+    iconAnchor: [21, 42],
+  });
   private readonly DEFAULT_LATLANG : LatLngTuple = [51.505, -0.09];
 
   @ViewChild('map', {static: true}) 
   mapRef !: ElementRef;
-
   map !: Map
+  currentMarker !: Marker;
+
+  private locationService = inject(LocationService);
 
   ngOnInit() : void {
     this.initializeMap();
@@ -29,9 +38,30 @@ export class MapComponent {
     }).setView(this.DEFAULT_LATLANG, 1);
 
     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    this.map.on('click', (e:LeafletMouseEvent) => {
+      this.setMarker(e.latlng);
+    })
   }
 
   findMyLocation() {
+    this.locationService.getCurrentLocation().subscribe({
+      next: (latlng) => { 
+        this.map.setView(latlng, this.MARKER_ZOOM_LEVEL);
+        this.setMarker(latlng);
+      }
+    });
+  }
+
+  setMarker(latlng: LatLngExpression) {
+    if (this.currentMarker) {
+      this.currentMarker.setLatLng(latlng);
+      return;
+    }
     
+    this.currentMarker = marker(latlng, {
+      draggable: true,
+      icon: this.MARKER_ICON
+    }).addTo(this.map);
   }
 }
