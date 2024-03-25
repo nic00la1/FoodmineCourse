fb5bd66e997a1c56e512fac4b5560cb38186787f
nic00la <nic00la.kaleta@gmail.com>
2024-03-25 09:26:03 +0100

    Continue Adding Order Model

--- backend/src/models/order.model.ts
+++ backend/src/models/order.model.ts
@@ -1,5 +1,6 @@
-import { Model, Schema, Types } from "mongoose";
+import { model, Schema, Types } from "mongoose";
 import { Food, FoodSchema } from "./food.models";
+import { OrderStatus } from "../constants/order_status";
 
 export interface LatLng {
   lat: number;
@@ -22,3 +23,38 @@ export const OrderItemSchema = new Schema<OrderItem>({
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
 });
+
+export interface Order {
+  id: number;
+  items: OrderItem[];
+  totalPrice: number;
+  name: string;
+  address: string;
+  addressLatLng: LatLng;
+  paymentId: string;
+  status: OrderStatus;
+  user: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const orderSchema = new Schema<Order>({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  addressLatLng: { type: LatLngSchema, required: true },
+  paymentId: { type: String, required: true },
+  items: { type: [OrderItemSchema], required: true },
+  status: { type: String, default: OrderStatus.NEW },
+  user: { type: Schema.Types.ObjectId, required: true },
+}, {
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+    },
+    toObject: {
+        virtuals: true,
+    }
+    // You have id instead of _id
+});
+
+export const OrderModel = model('order', orderSchema)
