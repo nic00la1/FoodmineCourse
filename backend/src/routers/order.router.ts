43944dd6fe44604197ca1e33bfdf1d2e9cec6426
nic00la <nic00la.kaleta@gmail.com>
2024-03-28 22:30:41 +0100

    COntinue adding order router

--- backend/src/routers/order.router.ts
+++ backend/src/routers/order.router.ts
@@ -3,8 +3,10 @@ import asyncHandler from 'express-async-handler';
 import { HTTP_BAD_REQUEST } from '../constants/http_status';
 import { OrderModel } from '../models/order.model';
 import { OrderStatus } from '../constants/order_status';
+import auth  from '../middlewares/auth.mid';
 
 const router = Router();
+router.use(auth);
 
 router.post('/create', 
 asyncHandler(async (req: any, res: any) => {
@@ -19,5 +21,11 @@ asyncHandler(async (req: any, res: any) => {
     await OrderModel.deleteOne({
         user: req.user.id,
         status: OrderStatus.NEW
-    })
+    });
+
+    const newOrder = new OrderModel({...requestOrder, user: req.user.id});
+    await newOrder.save();
+    res.send(newOrder);
 }))
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import express from "express";
 import cors from "cors";
 import foodRouter from "./routers/food.router";
 import userRouter from "./routers/user.router";
+import orderRouter from "./routers/order.router";
 import { dbConnect } from "./configs/database.config";
 dbConnect();
 
@@ -19,6 +20,7 @@ app.use(
 
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter );
 
 const port = 5000; // default port to listen
 app.listen(port, () => {
