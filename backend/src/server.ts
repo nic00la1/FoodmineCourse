bca8c61da4f52f658657d8552861fcb1dca60210
nic00la <nic00la.kaleta@gmail.com>
2024-03-29 19:16:16 +0100

    23. PUBLISH to Render.com & fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import foodRouter from "./routers/food.router";
 import userRouter from "./routers/user.router";
 import orderRouter from "./routers/order.router";
 import { dbConnect } from "./configs/database.config";
+import path from "path";
 dbConnect();
 
 const app = express();
@@ -17,6 +18,9 @@ app.use(
     origin: "http://localhost:4200",
   })
 );
+
+// Serve Angular files
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
