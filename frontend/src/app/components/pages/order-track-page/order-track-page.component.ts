18b35d8163fe7166b5cd3e7af880d499bbafa07b
nic00la <nic00la.kaleta@gmail.com>
2024-03-29 17:44:16 +0100

    Order Track page component HTML file

--- frontend/src/app/components/pages/order-track-page/order-track-page.component.ts
+++ frontend/src/app/components/pages/order-track-page/order-track-page.component.ts
@@ -2,11 +2,15 @@ import { Component, inject } from '@angular/core';
 import { Order } from '../../../shared/models/Order.model';
 import { ActivatedRoute } from '@angular/router';
 import { OrderService } from '../../../services/order/order.service';
+import { CommonModule } from '@angular/common';
+import { OrderItemListComponent } from '../../partials/order-item-list/order-item-list.component';
+import { MapComponent } from '../../partials/map/map.component';
+import { TitleComponent } from '../../partials/title/title.component';
 
 @Component({
   selector: 'app-order-track-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, OrderItemListComponent, MapComponent, TitleComponent],
   templateUrl: './order-track-page.component.html',
   styleUrl: './order-track-page.component.css'
 })
@@ -21,6 +25,7 @@ export class OrderTrackPageComponent {
 
     this.orderService.trackOrderById(params['orderId']).subscribe(order => {
       this.order = order;
+      this.order.totalPrice = order.items.reduce((total, item) => total + Number(item.price), 0);
     });
   }
 }
